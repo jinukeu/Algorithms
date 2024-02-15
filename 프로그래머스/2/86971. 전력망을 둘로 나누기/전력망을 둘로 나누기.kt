dc84b20b5c49514795574a578b0774f9ab27ffc0
이진욱 <koownij@kakao.com>
2024-02-15 12:30:04 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 22.62 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.kt
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.kt
@@ -1,50 +1,72 @@
-import java.util.*
-import kotlin.math.abs
-import kotlin.math.min
+import kotlin.math.*
+import kotlin.collections.*
 
 class Solution {
-fun solution(n: Int, wires: Array<IntArray>): Int {
-    var answer: Int = Int.MAX_VALUE
-
-    wires.forEach { (ra, rb) ->
+    fun solution(n: Int, wires: Array<IntArray>): Int {
+        var answer: Int = Int.MAX_VALUE
         val graph = MutableList(n + 1) { mutableListOf<Int>() }
-
+        
         wires.forEach { (a, b) ->
             graph[a].add(b)
             graph[b].add(a)
         }
-
-        graph[ra].remove(rb)
-        graph[rb].remove(ra)
-
-        val visited = MutableList(n + 1) { false }
-        val pq: Queue<Int> = LinkedList<Int>()
-
-        pq.add(ra)
-        visited[ra] = true
-        var c = 1
-
-        while (pq.isNotEmpty()) {
-            val n = pq.poll()
-            graph[n].forEach {
-                if(visited[it].not()) {
-                    c += 1
-                    visited[it] = true
-                    pq.add(it)
+        
+        /*
+        * 1 -> 3
+        * 2 -> 3
+        * 3 -> 1, 2, 4
+        * 4 -> 3, 5, 6, 7
+        * 5 -> 4
+        * 6 -> 4
+        * 7 -> 4, 8, 9
+        * 8 -> 7
+        * 9 -> 7
+        */
+        
+        wires.forEach { (a, b) ->
+            var ac = 0
+            var bc = 0
+            
+            val q = ArrayDeque<Int>()
+            val visited = MutableList<Boolean>(n + 1) { false }
+            visited[b] = true
+            q.add(a)
+            ac += 1
+            while (q.isNotEmpty()) {
+                val next = q.removeFirst()
+                if (visited[next]) continue
+                visited[next] = true
+                graph[next].forEach { n ->
+                    if (visited[n].not()) {
+                        ac += 1
+                        q.add(n)
+                    } 
                 }
             }
+            
+            val q2 = ArrayDeque<Int>()
+            val visited2 = MutableList<Boolean>(n + 1) { false }
+            visited2[a] = true
+            q2.add(b)
+            bc += 1
+            while (q2.isNotEmpty()) {
+                val next = q2.removeFirst()
+                if (visited2[next]) continue
+                visited2[next] = true
+                graph[next].forEach { n ->
+                    if (visited2[n].not()) {
+                        bc += 1
+                        q2.add(n)
+                    } 
+                }
+            }
+            
+            //println("a = $a b = $b")
+            //println("ac = $ac bc = $bc")
+            //println("---")
+            answer = min(abs(ac - bc), answer)
         }
-
-
-        answer = min(answer, abs(n - c - c))
-
-//        println("$ra $rb 끊음")
-//        graph.forEach { println(it) }
-//        println("${c} - ${n - c} =  ${abs(n - c - c)}")
-//        println("---")
-
-
-    }
+        
         return answer
-}
+    }
 }
